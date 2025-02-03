390875e165d9836e5e5cde4216dbd2e505412ff7
scrosanne <sc.rosanne@gmail.com>
2025-02-03 17:30:34 +0100

    prisma create user

--- src/actions/addEntry.ts
+++ src/actions/addEntry.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { formSchema, FormSchemaType } from '@/schema/formSchema';
+import { prisma } from '../../lib/prisma';
 
 export async function addEntry(data: FormSchemaType) {
   await new Promise(resolve => setTimeout(resolve, 1000));
@@ -10,6 +11,17 @@ export async function addEntry(data: FormSchemaType) {
   console.log(result);
 
   if (result.success) {
+    const user = await prisma.user.create({
+      data: {
+        name: result.data.name,
+        email: result.data.email,
+        password: result.data.password
+      }
+    });
+
+    const users = await prisma.user.findMany();
+    console.log('new users', users);
+
     return {
       success: true,
       data: result.data
